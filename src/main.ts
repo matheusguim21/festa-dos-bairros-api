078b994d49b44ba754ec2cedaefb61fae6ee70c2
matheusguim21 <matheusguim13@gmail.com>
2025-05-28 14:36:23 -0300

    adding host 0.0.0.0 ao listenm

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,6 @@ async function bootstrap() {
 
   const port = configService.get("PORT", { infer: true });
   app.enableCors();
-  await app.listen(port);
+  await app.listen(port, "0.0.0.0");
 }
 bootstrap();
